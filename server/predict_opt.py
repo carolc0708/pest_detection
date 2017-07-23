e4a36b147bfdf79f63b44c3eb21dcf21ed17ea10
carolc0708 <carolc830708@gmail.com>
2017-07-23 18:10:59 +0800

    add optimized version for predict.py

---
+++ server/predict_opt.py
@@ -0,0 +1,62 @@
+from keras.models import Sequential
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+from keras.layers import Dropout, Flatten, Dense
+import os
+from twisted.internet.defer import returnValue
+from autobahn.twisted.wamp import Application
+
+
+app = Application(u'io.crossbar.examples.pi.camera')
+# dimensions of our images.
+img_width, img_height = 150, 150
+curdir_path = os.path.dirname(os.path.realpath(__file__))
+img_path = curdir_path + '/copy.jpg'
+
+# load VGG16 neuronal network model from keras.applications
+# model = VGG16(weights='imagenet', include_top=False)
+model = VGG16(weights=None, include_top=False)
+# load VGG16 pre-train weights
+model.load_weights(curdir_path + '/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5')
+
+# set photo need to predict
+img = image.load_img(img_path, target_size=(227, 227))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+# use VGG16 model to extract features
+features = model.predict(x)
+
+# set classifier network model
+bottleneck_model = Sequential()
+bottleneck_model.add(Flatten(input_shape=features.shape[1:]))
+bottleneck_model.add(Dense(256, activation='relu'))
+bottleneck_model.add(Dropout(0.5))
+bottleneck_model.add(Dense(4, activation='softmax'))
+bottleneck_model.load_weights(curdir_path + '/fc_model.h5')
+del img, x, features
+
+@app.register(u'io.crossbar.examples.pi.camera.predict')
+def predict():
+    # execute classification
+    img = image.load_img(img_path, target_size=(227, 227))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    features = model.predict(x)
+    preds = bottleneck_model.predict_classes(features, verbose=0)
+    del img, x, features
+    print(str(preds[0]))
+    return str(preds[0])
+
+
+@app.signal('onjoined')
+def onjoined():
+    print("realm joined!")
+
+
+if __name__ == "__main__":
+    app.run(url=u'ws://localhost:8080/ws', realm=u"realm1")
---
+++ server/takePhoto_n_Predict_opt.js
@@ -0,0 +1,182 @@
+var autobahn = require('autobahn');
+var fs = require('fs');
+var child_process = require('child_process');
+
+
+var base64_decode = function(base64str, file) {
+    // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
+    var bitmap = new Buffer(base64str, 'base64');
+    // write buffer to file
+    fs.writeFileSync(file, bitmap);
+    console.log('******** File created from base64 encoded string ********');
+}
+
+var when = autobahn.when;
+var session = null;
+
+function main(){
+    console.log('Server get Photo starting')
+    var curdir = require('path').dirname(require.main.filename);
+
+    var connection = new autobahn.Connection({
+//        url: 'ws://140.116.226.234:8080/ws', // replace with URL of WAMP router if this doesn't serve the file
+        url: 'ws://localhost:8080/ws', // replace with URL of WAMP router if this doesn't serve the file
+        realm: "realm1"
+    });
+
+//    var predictPhoto = function (args, kwargs, details) {
+//
+//        console.log("takePicture&predict called");
+//
+//        if (details.progress) {
+//            details.progress(["takePhoto&predict called"]);
+//        }
+//
+//        var predictResult = when.defer();
+//        var result = {};
+//
+//        session.call("io.crossbar.examples.pi.camera.take_photo").then(
+//            function (res) {
+//                result['base64image'] = res;
+//                // need to remove the header and footer which uuencode adds
+//                res = res.slice(29);
+//                res = res.slice(0, -6);
+//
+//                base64_decode(res, curdir + '/copy.jpg');
+//                console.log('Get Photo successfully!')
+//                var cmd = 'python2.7 '+curdir+'/predict.py';
+//                child_process.exec(cmd, function(error, stdout, stderr) {
+//                    // command output is in stdout
+//                    console.log(stdout.trim());
+//                    switch(stdout.trim()) {
+//                        case '0':
+//                            result['predict'] = '薊馬'; // black
+//                            break;
+//                        case '1':
+//                            result['predict'] = '蛾類幼蟲'; // moth
+//                            break;
+//                        case '2':
+//                            result['predict'] = '粉蝨'; // pink
+//                            break;
+//                        case '3':
+//                            result['predict'] = '葉蟎紅蜘蛛'; // red
+//                            break;
+//                        default:
+//                            result['predict'] = stdout;
+//                            break;
+//                    }
+//
+//                    console.log('Predict successfully!');
+//                    predictResult.resolve(result);
+//                });
+//
+//            },
+//            function (err) {
+//                console.log("requestImage failed", err);
+//                imageProgress.innerHTML = "Error getting image!";
+//            }
+//        );
+//
+//        return predictResult.promise;
+//    };
+
+    // fired when connection is established and session attached
+    //
+    connection.onopen = function (sess, details) {
+
+        session = sess;
+
+        console.log("connected");
+
+        function getPhoto (args, kwargs) {
+            var result = {};
+            result['base64image'] = kwargs.photo;
+            result['time'] = kwargs.time;
+            console.log(result.time);
+            // need to remove the header and footer which uuencode adds
+            result['base64image'] = result['base64image'].slice(29);
+            result['base64image'] = result['base64image'].slice(0, -6);
+
+            base64_decode(result['base64image'], curdir + '/copy.jpg');
+            console.log('Get Photo successfully!');
+            session.call("io.crossbar.examples.pi.camera.predict").then(
+               function (res) {
+                  console.log(res);
+                  switch(res.trim()) {
+                       case '0':
+                           result['predict'] = '薊馬'; // black
+                           break;
+                       case '1':
+                           result['predict'] = '蛾類幼蟲'; // moth
+                           break;
+                       case '2':
+                           result['predict'] = '粉蝨'; // pink
+                           break;
+                       case '3':
+                           result['predict'] = '葉蟎紅蜘蛛'; // red
+                           break;
+                       default:
+                           result['predict'] = res;
+                           break;
+                   }
+
+                  console.log('這是' + result.predict);
+
+                  session.publish("io.crossbar.examples.pi.camera.take_photo_n_predict", [], result);
+              },
+              function (err) {
+                 console.log(err);
+              }
+            );
+//            var cmd = 'python2.7 '+curdir+'/predict.py';
+//            child_process.exec(cmd, function(error, stdout, stderr) {
+//                // command output is in stdout
+//                console.log(stdout.trim());
+//                switch(stdout.trim()) {
+//                    case '0':
+//                        result['predict'] = '薊馬'; // black
+//                        break;
+//                    case '1':
+//                        result['predict'] = '蛾類幼蟲'; // moth
+//                        break;
+//                    case '2':
+//                        result['predict'] = '粉蝨'; // pink
+//                        break;
+//                    case '3':
+//                        result['predict'] = '葉蟎紅蜘蛛'; // red
+//                        break;
+//                    default:
+//                        result['predict'] = stdout;
+//                        break;
+//                }
+//
+//                console.log('這是' + result.predict);
+//
+//                session.publish("io.crossbar.examples.pi.camera.take_photo_n_predict", [], result);
+//            });
+        }
+
+        session.subscribe('io.crossbar.examples.pi.camera.take_photo', getPhoto);
+//        session.register("io.crossbar.examples.pi.camera.take_photo_n_predict", predictPhoto).then(
+//            function (registration) {
+//                console.log("Procedure 'io.crossbar.examples.pi.camera.take_photo_n_predict' registered:", registration.id);
+//            },
+//            function (error) {
+//                console.log("Registration failed:", error);
+//            }
+//        );
+
+    };
+
+    // fired when connection was lost (or could not be established)
+    //
+    connection.onclose = function (reason, details) {
+
+        console.log("Connection lost: " + reason);
+
+    }
+
+    connection.open()
+}
+
+main();
